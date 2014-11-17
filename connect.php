d20cd44b9b95d7708816363a31d784e6f71ffc68
Ahmed Karanath <ahmed_k@students.iitmandi.ac.in>
2014-11-17 06:51:36 +0530

    Moved files for stting up git

--- connect.php
+++
@@ -1,8 +0,0 @@
-	<?php
-		$host="localhost";
-		$user="root";
-		$password="root";
-		$db="craigslist";
-		$connect=mysql_connect($host,$user,$password) or die(mysql_error());
-		mysql_select_db($db,$connect);
-	?>
--- core.php
+++
@@ -1,3 +0,0 @@
-<?php
- $scriptname=$_SERVER['SCRIPT_NAME'];
-?>
