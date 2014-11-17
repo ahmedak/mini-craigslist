133394f94ee6c3db6356aed6a6ccc4f0e9ade445
Ahmed Karanath <ahmed_k@students.iitmandi.ac.in>
2014-11-17 09:42:28 +0530

    Check if user exists and insert user data into database

--- signup.php
+++ signup.php
@@ -12,4 +12,22 @@
 	$email=$_POST[email];
 
 	//encrypt password using sha
-	$password=sha256($password)
+	$hashed_password=hash(sha1($password));
+
+	//check if user already exists
+	$sql0="SELECT * FROM users WHERE username= ? OR emailid= ?";
+	$params0 = array($username,$emailid);
+	$stmt0= sqlsrv_query($connect, $sql0, $params0);
+	if(sqlsrv_has_rows($stmt)) { 
+		echo "User exists with same username and/or email address. Please select another username or use a different email address."
+		header('Location: /signup.html');
+	}
+
+	//insert user into database
+	$sql="INSERT INTO users (fname,lname,sex,username,emailid,contactno,password) values (?,?,?,?,?,?,?)";
+	$params= array($fname,$lname,$gender,$username,$email,$contactno,$hashed_password);
+	$stmt = sqlsrv_query($connect, $sql, $params);
+	if(sqlsrv_has_rows($stmt)) { 
+    	echo "Welcome, ".$fname; 
+    	header('Location: /index.html');
+	}
